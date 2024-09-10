e8b64e3701b9f9f39b7ea1b43570b28a9d733d5a
AmyangXYZ <amyang.xyz@gmail.com>
2024-09-10 17:48:45 -0400

    adding icons

--- src/components/ComputerDesk.vue
+++ src/components/ComputerDesk.vue
@@ -1,8 +1,38 @@
 <template>
-  <el-card class="desk"> </el-card>
+  <el-card class="desk">
+    <el-row align="middle">
+      <el-col :span="4" align="center">
+        <el-icon size="64">
+          <Headset />
+        </el-icon>
+      </el-col>
+      <el-col :span="6" align="left">
+        <el-icon size="48">
+          <MilkTea />
+        </el-icon>
+      </el-col>
+      <el-col :span="4" align="center">
+        <el-icon size="96">
+          <IconKeyboard />
+        </el-icon>
+      </el-col>
+      <el-col :span="4" align="center">
+        <el-icon size="48">
+          <Mouse />
+        </el-icon>
+      </el-col>
+      <el-col :span="4" align="right">
+        <el-icon size="48">
+          <Iphone />
+        </el-icon>
+      </el-col>
+    </el-row>
+  </el-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import IconKeyboard from '@/components/icons/IconKeyboard.vue'
+</script>
 
 <style scoped>
 .desk {
