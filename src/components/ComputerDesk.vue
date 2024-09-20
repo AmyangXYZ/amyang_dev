335a784ccaccfa20886ce9ddea47d43f0ca751e3
AmyangXYZ <amyang.xyz@gmail.com>
2024-09-20 09:44:32 -0400

    change link from aether-gazer to mikapo

--- src/components/ComputerDesk.vue
+++ src/components/ComputerDesk.vue
@@ -39,7 +39,7 @@
         <IconKeyboard />
       </el-col>
       <el-col :span="7" align="right">
-        <a href="https://aether-gazer.amyang.dev" target="_blank">
+        <a href="https://mikapo.amyang.dev" target="_blank">
           <el-icon size="48" id="mmd" color="orange">
             <IconChild />
           </el-icon>
